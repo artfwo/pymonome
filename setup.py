82a8cd3855b8775111fb6ee9219216b7b1e2701a
rknLA <kevin@rkn.la>
2013-01-01 22:24:54 -0800

    Include dependencies in setup.

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ setup(
     name='pymonome',
     version='0.2',
     packages=['monome', ],
+    license='COPYING',
+    url='http://pypi.python.org/pypi/pymonome',
+    description='a monome serialosc client in python',
     long_description=open('README').read(),
+    install_requires=[
+        "pyOSC >= 0.3",
+        "pybonjour >= 1.1.1",
+    ],
 )
