a0e5ca80439021193f70a7f60d967529a892b250
Artem Popov <artfwo@gmail.com>
2017-10-29 15:23:13 +0700

    update life example

--- examples/life.py
+++ examples/life.py
@@ -6,52 +6,55 @@ import asyncio
 import monome
 import copy, itertools, random
 
-class Life(monome.Monome):
+class Life(monome.App):
     def __init__(self):
-        super().__init__('/life')
+        super().__init__() # TODO: prefix
         self.alive = True
 
-    def ready(self):
-        self.world = [[0 for col in range(self.width)] for row in range(self.height)]
+    def on_grid_ready(self):
+        self.world = [[0 for col in range(self.grid.width)] for row in range(self.grid.height)]
+        self.randomize()
         self.task = asyncio.async(self.begin())
 
-    def disconnect(self):
-        super().disconnect()
+    def on_grid_disconnect(self):
         self.task.cancel()
-        self.led_all(0)
 
-    def grid_key(self, x, y, s):
-        if x == self.width - 1 and y == 0 and s == 1:
+    def on_grid_key(self, x, y, s):
+        if x == self.grid.width - 1 and y == 0 and s == 1:
             self.alive = not self.alive
-            self.led_set(self.width - 1, 0, int(not self.alive))
+            self.grid.led_set(self.grid.width - 1, 0, int(not self.alive))
             return
         if s == 1:
             row, col = y, x
             self.world[row][col] ^= 1
-            self.led_set(x, y, self.world[row][col])
-        else:
-            pass
+            self.grid.led_set(x, y, self.world[row][col])
+
+    def quit(self):
+        self.task.cancel()
 
     async def begin(self):
-        while True:
-            if self.alive:
-                self.update()
-                for i, row in enumerate(self.world):
-                    self.led_row(0, i, row)
-            await asyncio.sleep(0.2)
+        try:
+            while True:
+                if self.alive:
+                    self.update()
+                    for i, row in enumerate(self.world):
+                        self.grid.led_row(0, i, row)
+                await asyncio.sleep(0.2)
+        except asyncio.CancelledError:
+            pass
 
     def update(self):
         def neighbors(x, y):
             result = 0
             for dx, dy in [d for d in itertools.product([-1, 0, 1], repeat=2) if d != (0, 0)]:
-                row = (y + dy) % self.height
-                col = (x + dx) % self.width
+                row = (y + dy) % self.grid.height
+                col = (x + dx) % self.grid.width
                 result += self.world[row][col]
             return result
 
         new_world = copy.deepcopy(self.world)
-        for x in range(self.width):
-            for y in range(self.height):
+        for x in range(self.grid.width):
+            for y in range(self.grid.height):
                 n = neighbors(x, y)
                 row, col = y, x
                 if n == 3:
@@ -61,11 +64,16 @@ class Life(monome.Monome):
         self.world = copy.deepcopy(new_world)
 
     def randomize(self):
-        for row in range(self.height):
-            for col in range(self.width):
+        for row in range(self.grid.height):
+            for col in range(self.grid.width):
                 self.world[row][col] = random.randint(0, 1)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    asyncio.async(monome.create_serialosc_connection(Life), loop=loop)
-    loop.run_forever()
+    life_app = Life()
+    asyncio.async(monome.SerialOsc.create(loop=loop, autoconnect_app=life_app))
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        life_app.quit()
