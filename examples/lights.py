4bf73f744f8f6bbe2d0275d65632d4eda03e0b4a
Artem Popov <artfwo@gmail.com>
2019-10-06 18:54:30 +0200

    update lights.py

--- examples/lights.py
+++ examples/lights.py
@@ -4,38 +4,49 @@ import random
 import asyncio
 import monome
 
-class Lights(monome.Monome):
+class Lights(monome.GridApp):
     def __init__(self):
-        super().__init__('/lights')
+        super().__init__()
+        self.task = asyncio.ensure_future(asyncio.sleep(0))
 
-    def ready(self):
+    def on_grid_ready(self):
         self.alive = True
-        asyncio.async(self.animate())
+        self.task = asyncio.ensure_future(self.animate())
 
-    def grid_key(self, x, y, s):
+    def on_grid_key(self, x, y, s):
         if s == 1:
-            self.led_set(x, y, s)
+            self.grid.led_set(x, y, s)
 
-    def disconnect(self, *args):
+    def on_grid_disconnect(self, *args):
         self.alive = False
-        super().disconnect(*args)
+        self.task.cancel()
 
     async def animate(self):
         while self.alive:
-            for i in range(self.height):
-                row = [random.randint(0, 1) for i in range(self.width)]
+            for i in range(self.grid.height):
+                row = [random.randint(0, 1) for i in range(self.grid.width)]
                 for j in range(3):
-                    self.led_row(0, i, [0] * self.width)
+                    self.grid.led_row(0, i, [0] * self.grid.width)
                     await asyncio.sleep(1 / 30)
-                    self.led_row(0, i, row)
+                    self.grid.led_row(0, i, row)
                     await asyncio.sleep(1 / 30)
-            for i in reversed(range(self.height)):
-                row = [random.randint(0, 1) for i in range(self.width)]
-                self.led_row(0, i, row)
+            for i in reversed(range(self.grid.height)):
+                row = [random.randint(0, 1) for i in range(self.grid.width)]
+                self.grid.led_row(0, i, row)
                 await asyncio.sleep(1 / 20)
             await asyncio.sleep(2)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    asyncio.async(monome.create_serialosc_connection(Lights), loop=loop)
+    lights = Lights()
+
+    def serialosc_device_added(id, type, port):
+        print('connecting to {} ({})'.format(id, type))
+        asyncio.ensure_future(lights.grid.connect('127.0.0.1', port))
+
+    serialosc = monome.SerialOsc()
+    serialosc.device_added_event.add_handler(serialosc_device_added)
+
+    loop.run_until_complete(serialosc.connect())
+
     loop.run_forever()
