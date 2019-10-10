1912f7416343285cb552913eb0f771ec51ce0c12
Artem Popov <artfwo@gmail.com>
2019-10-10 23:00:30 +0200

    update splitter example

--- examples/splitter.py
+++ examples/splitter.py
@@ -27,16 +27,26 @@ class SplitterSerialOsc(monome.SerialOsc):
         if type == "monome 128":
             asyncio.async(self.splitter_connect(port))
 
-if __name__ == "__main__":
-    life1 = Life()
-    life2 = Life()
-
+if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    asyncio.async(SplitterSerialOsc.create(loop=loop, app1=life1, app2=life2))
 
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        life1.quit()
-        life2.quit()
+    section1 = monome.GridSection((8, 8), (0, 0))
+    section2 = monome.GridSection((8, 8), (8, 0))
+    splitter = monome.GridSplitter([section1, section2])
 
+    life1 = Life()
+    life1.set_grid(section1)
+
+    life2 = Life()
+    life2.set_grid(section2)
+
+    def serialosc_device_added(id, type, port):
+        print('connecting to {} ({})'.format(id, type))
+        asyncio.ensure_future(splitter.grid.connect('127.0.0.1', port))
+
+    serialosc = monome.SerialOsc()
+    serialosc.device_added_event.add_handler(serialosc_device_added)
+
+    loop.run_until_complete(serialosc.connect())
+
+    loop.run_forever()
