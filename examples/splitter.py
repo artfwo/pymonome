cdfc3a3d6631556227709fdd2affe8fca80115b9
Artem Popov <artfwo@gmail.com>
2019-10-10 23:01:08 +0200

    remove dead code from splitter example

--- examples/splitter.py
+++ examples/splitter.py
@@ -5,28 +5,6 @@ import monome
 
 from life import Life
 
-class SplitterSerialOsc(monome.SerialOsc):
-    def __init__(self, app1, app2, loop=None, autoconnect_app=None):
-        super().__init__(loop, autoconnect_app)
-        self.app1 = app1
-        self.app2 = app2
-
-    async def splitter_connect(self, port):
-        transport, grid = await self.loop.create_datagram_endpoint(monome.Grid, local_addr=('127.0.0.1', 0), remote_addr=('127.0.0.1', port))
-
-        section1 = monome.GridSection((8, 8), (0, 0))
-        section2 = monome.GridSection((8, 8), (8, 0))
-        splitter = monome.Splitter(grid, [section1, section2])
-
-        self.app1.attach(section1)
-        self.app2.attach(section2)
-
-        splitter.connect()
-
-    def on_device_added(self, id, type, port):
-        if type == "monome 128":
-            asyncio.async(self.splitter_connect(port))
-
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
 
