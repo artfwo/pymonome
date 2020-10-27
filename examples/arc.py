d1d980c57bda0d122b42d679d054c06c98710c9c
Joshua Subramaniam <SuuBro@users.noreply.github.com>
2020-10-27 19:34:34 +0000

    PEP8

--- examples/arc.py
+++ examples/arc.py
@@ -6,25 +6,25 @@ import asyncio
 import monome
 
 
-class ExampleArcApp(monome.ArcApp):   
+class ExampleArcApp(monome.ArcApp):
     def __init__(self):
         super().__init__()
         self.pos = [0, 0, 0, 0]
-        
+
     def on_arc_ready(self):
         print('Ready, clearing all rings...')
-        for n in range(0,4):
+        for n in range(0, 4):
             self.arc.ring_all(n, 0)
-            
+
     def on_arc_disconnect(self):
         print('Arc disconnected.')
-        
+
     def on_arc_delta(self, ring, delta):
         print('Ring: {} Delta: {}'.format(ring, delta))
-        
+
         old_pos = self.pos[ring]
         new_pos = old_pos + delta
-        
+
         if new_pos > old_pos:
             for p in range(old_pos, new_pos):
                 self.arc.ring_set(ring, p, 15)
@@ -36,7 +36,7 @@ class ExampleArcApp(monome.ArcApp):
     def on_arc_key(self, ring, s):
         print('Ring: {} Pressed:{}'.format(ring, s > 0))
         self.arc.ring_all(ring, 15 if s > 0 else 0)
-        
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
