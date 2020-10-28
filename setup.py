0ce6872d8b44f25bb46d770fda5690a5805c5cca
Artem Popov <artfwo@gmail.com>
2020-10-28 10:02:56 +0100

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/artfwo/pymonome',
     description='a monome serialosc client in python',
     long_description=long_description,
-    version='0.10.0',
+    version='0.10.1',
     py_modules=['monome'],
     include_package_data=True,
     install_requires=[
