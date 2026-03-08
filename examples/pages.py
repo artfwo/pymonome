303e5409f827e7afc544ec845e5624004b693f5b
Artem Popov <art@artfwo.net>
2026-03-08 16:14:13 +0100

    cleanup pages example

--- examples/pages.py
+++ examples/pages.py
@@ -5,27 +5,6 @@ import monome
 
 from life import Life
 
-class PagesSerialOsc(monome.SerialOsc):
-    def __init__(self, app1, app2, loop=None, autoconnect_app=None):
-        super().__init__(loop, autoconnect_app)
-        self.app1 = app1
-        self.app2 = app2
-
-    async def pages_connect(self, port):
-        transport, grid = await self.loop.create_datagram_endpoint(monome.Grid, local_addr=('127.0.0.1', 0), remote_addr=('127.0.0.1', port))
-
-        page1 = monome.Page()
-        page2 = monome.Page()
-        page_manager = monome.SeqPageManager(grid=grid, pages=[page1, page2])
-
-        self.app1.attach(page1)
-        self.app2.attach(page2)
-
-        page_manager.connect()
-
-    def on_device_added(self, id, type, port):
-        asyncio.create_task(self.pages_connect(port))
-
 async def main():
     loop = asyncio.get_running_loop()
 
